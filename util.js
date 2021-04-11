6486cdc4f1cff6aecce56d72588d35a1d96add2b
baixinxin <baixinxin518@163.com>
2021-04-11 08:26:33 +0800

    add polyfill.js fix util.js

--- util.js
+++ util.js
@@ -18,3 +18,16 @@
 
 			return undefined;
 		})();
+    
+		// js类型检查
+		var Type = (function(){
+			var Type = {}
+      for(var i = 0, type; type =['String', 'Array', 'Number', 'Object', 'Array', 'Function', 'Date', 'RegExp'][i++];) {
+				(function(type){
+					Type['is' + type] = function(obj) {
+						return Object.prototype.toString.call(obj) === '[object '+ type +']'
+					}
+				})(type)
+			}
+			return Type
+		})()
